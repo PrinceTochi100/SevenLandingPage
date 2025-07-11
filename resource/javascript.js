9005a3c56f9591f6b4909b659316266db54f32a5
PrinceTochi100 <okoronkwoprince92@gmail.com>
2025-07-11 12:53:38 +0100

    main

--- resource/javascript.js
+++ resource/javascript.js
@@ -4,3 +4,21 @@ const navMenu = document.querySelector('.nav-menu');
 toggleBtn.addEventListener('click', () => {
     navMenu.classList.toggle('active')
 });
+
+//progress bar section
+const progressFills = document.querySelectorAll('.progress-fill');
+
+const animateOnScroll = () => {
+    progressFills.forEach(fill => {
+        const rect = fill.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (rect.top < windowHeight - 50 && !fill.classList.contains('animated')) {
+            fill.style.width = fill.getAttribute('data-percent') + '%';
+            fill.classList.add('animated'); // Prevent retrigger
+        }
+    });
+};
+
+window.addEventListener('scroll', animateOnScroll);
+window.addEventListener('load', animateOnScroll);
